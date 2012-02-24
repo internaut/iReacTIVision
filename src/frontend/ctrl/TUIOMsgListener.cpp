0996076ffa51bbc1df6748803020c572da2633bf
Markus Konrad <post@mkonrad.net>
2012-02-24 20:44:35 +0100

    implemented tuio message parsing works

--- src/frontend/ctrl/TUIOMsgListener.cpp
+++ src/frontend/ctrl/TUIOMsgListener.cpp
@@ -11,46 +11,147 @@
 #include "TUIOMsgListener.h"
 
 TUIOMsgListener::TUIOMsgListener(TUIOMsgCallback callback) :
-    msgCallback(callback) {
+    msgCallback(callback),
+    tuioAddressPrefix("/tuio") {
     
 }
 
+void TUIOMsgListener::setTUIOAddressPrefix(const char *newPrefix) {
+    tuioAddressPrefix = newPrefix;
+}
+
 void TUIOMsgListener::ProcessMessage(const osc::ReceivedMessage &m, const IpEndpointName &remoteEndpoint) {
-    try{
+    try {
+        const char *addr = m.AddressPattern();
+        
         std::cout << "TUIOMsgListener: Received message "
-            << m.AddressPattern() << std::endl;
-    
-//        // example of parsing single messages. osc::OscPacketListener
-//        // handles the bundle traversal.
-//        if( strcmp( m.AddressPattern(), "/test1" ) == 0 ){
-//            // example #1 -- argument stream interface
-//            osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-//            bool a1;
-//            osc::int32 a2;
-//            float a3;
-//            const char *a4;
-//            args >> a1 >> a2 >> a3 >> a4 >> osc::EndMessage;
-//
-//            std::cout << "received '/test1' message with arguments: "
-//                << a1 << " " << a2 << " " << a3 << " " << a4 << "\n";                              
-//
-//        }else if( strcmp( m.AddressPattern(), "/test2" ) == 0 ){
-//            // example #2 -- argument iterator interface, supports
-//            // reflection for overloaded messages (eg you can call
-//            // (*arg)->IsBool() to check if a bool was passed etc).
-//
-//            osc::ReceivedMessage::const_iterator arg = m.ArgumentsBegin();
-//            bool a1 = (arg++)->AsBool();
-//            int a2 = (arg++)->AsInt32();
-//            float a3 = (arg++)->AsFloat();
-//            const char *a4 = (arg++)->AsString();
-//            if( arg != m.ArgumentsEnd() )
-//                throw osc::ExcessArgumentException();
-//
-//            std::cout << "received '/test2' message with arguments: "
-//                << a1 << " " << a2 << " " << a3 << " " << a4 << "\n";
-//        }
-    }catch( osc::Exception& e ){
+            << addr << std::endl;
+            
+        const int prefixLen = strlen(tuioAddressPrefix);
+        
+        if (strncmp(addr, tuioAddressPrefix, prefixLen) == 0 && strlen(addr) > prefixLen + 1) {
+            TUIOMsg msg;
+            const char *type = (addr + prefixLen + 1);
+            osc::ReceivedMessage::const_iterator arg = m.ArgumentsBegin();
+            const unsigned long numArgs = m.ArgumentCount();
+
+            if (strcmp(type, "2Dobj") == 0) {
+                msg.type = kTUIOMsgTypeObj;
+            } else if (strcmp(type, "2Dcur") == 0) {
+                msg.type = kTUIOMsgTypeCur;
+            } else if (strcmp(type, "2Dblb") == 0) {
+                msg.type = kTUIOMsgTypeBlb;
+            } else {
+                std::cerr << "TUIOMsgListener: unknown TUIO message type "
+                    << type << std::endl;
+                return;
+            }
+            
+            const char *cmd = (arg++)->AsString();
+
+            if (strcmp(cmd, "source") == 0) {
+                msg.cmd = kTUIOMsgCmdSrc;
+            } else if (strcmp(cmd, "alive") == 0) {
+                msg.cmd = kTUIOMsgCmdAlive;
+            } else if (strcmp(cmd, "set") == 0) {
+                msg.cmd = kTUIOMsgCmdSet;
+            } else if (strcmp(cmd, "fseq") == 0) {
+                msg.cmd = kTUIOMsgCmdFSeq;
+            } else {
+                std::cerr << "TUIOMsgListener: unknown TUIO message command "
+                    << cmd << std::endl;
+                return;
+            }
+            
+            switch (msg.cmd) {
+                case kTUIOMsgCmdSrc: {
+                    const char *srcAddr = (arg++)->AsString();
+                    const int addrLen = strlen(srcAddr + 1);
+                    msg.data.source.addr = new char[addrLen];
+                    strncpy(msg.data.source.addr, srcAddr, addrLen);
+                }
+                break;
+
+                case kTUIOMsgCmdAlive: {
+                    msg.data.alive.numSessIds = numArgs - 1;
+                    if (msg.data.alive.numSessIds > 0) {
+                        msg.data.alive.sessIds = new int[msg.data.alive.numSessIds];
+                        
+                        for (int i = 0; i < msg.data.alive.numSessIds; i++) {
+                            msg.data.alive.sessIds[i] = (arg++)->AsInt32();
+                        }
+                    } else {
+                        msg.data.alive.sessIds = NULL;
+                    }
+                }
+                break;
+                
+                case kTUIOMsgCmdSet: {
+                    msg.data.set.sessId = (arg++)->AsInt32();
+                
+                    switch (msg.type) {
+                        case kTUIOMsgTypeObj:
+                            msg.data.set.classId = (arg++)->AsInt32();
+                            msg.data.set.pos.x = (arg++)->AsFloat();
+                            msg.data.set.pos.y = (arg++)->AsFloat();
+                            msg.data.set.angle = (arg++)->AsFloat();
+                            msg.data.set.size.x = 0.0f;
+                            msg.data.set.size.y = 0.0f;
+                            msg.data.set.area = 0.0f;
+                            msg.data.set.vel.x = (arg++)->AsFloat();
+                            msg.data.set.vel.y = (arg++)->AsFloat();
+                            msg.data.set.angleVel = (arg++)->AsFloat();
+                            msg.data.set.motAccel = (arg++)->AsFloat();
+                            msg.data.set.rotAccel = (arg++)->AsFloat();
+                        break;
+                        
+                        case kTUIOMsgTypeCur:
+                            msg.data.set.classId = 0;
+                            msg.data.set.pos.x = (arg++)->AsFloat();
+                            msg.data.set.pos.y = (arg++)->AsFloat();
+                            msg.data.set.angle = 0.0f;
+                            msg.data.set.size.x = 0.0f;
+                            msg.data.set.size.y = 0.0f;
+                            msg.data.set.area = 0.0f;
+                            msg.data.set.vel.x = (arg++)->AsFloat();
+                            msg.data.set.vel.y = (arg++)->AsFloat();
+                            msg.data.set.angleVel = 0.0f;
+                            msg.data.set.motAccel = (arg++)->AsFloat();
+                            msg.data.set.rotAccel = 0.0f;
+                        break;
+                        
+                        case kTUIOMsgTypeBlb:
+                            msg.data.set.classId = 0;
+                            msg.data.set.pos.x = (arg++)->AsFloat();
+                            msg.data.set.pos.y = (arg++)->AsFloat();
+                            msg.data.set.angle = (arg++)->AsFloat();
+                            msg.data.set.size.x = (arg++)->AsFloat();
+                            msg.data.set.size.y = (arg++)->AsFloat();
+                            msg.data.set.area = 0.0f;
+                            msg.data.set.vel.x = (arg++)->AsFloat();
+                            msg.data.set.vel.y = (arg++)->AsFloat();
+                            msg.data.set.angleVel = (arg++)->AsFloat();
+                            msg.data.set.motAccel = (arg++)->AsFloat();
+                            msg.data.set.rotAccel = (arg++)->AsFloat();
+                        break;
+                    }
+                }   
+                break;
+                
+                case kTUIOMsgCmdFSeq: {
+                    msg.data.fseq.frameId = (arg++)->AsInt32();
+                }
+                break;
+            }
+            
+            if (msgCallback) {
+                msgCallback(msg);
+            }
+        } else {
+            std::cerr << "TUIOMsgListener: Address prefix " << addr << " did not match to "
+                << tuioAddressPrefix << std::endl;
+        }
+    } catch( osc::Exception& e ){
         // any parsing errors such as unexpected argument types, or
         // missing arguments get thrown as exceptions.
         std::cerr << "TUIOMsgListener: error while parsing message "
--- src/frontend/ctrl/TUIOMsgListener.h
+++ src/frontend/ctrl/TUIOMsgListener.h
@@ -11,19 +11,81 @@
 #ifndef iReacTIVision_TUIOMsgListener_h
 #define iReacTIVision_TUIOMsgListener_h
 
-typedef const char*(*TUIOMsgCallback)(void);
+typedef enum {
+    kTUIOMsgTypeObj = 0,
+    kTUIOMsgTypeCur,
+    kTUIOMsgTypeBlb
+} TUIOMsgType;
+
+typedef enum {
+    kTUIOMsgCmdSrc = 0,
+    kTUIOMsgCmdAlive,
+    kTUIOMsgCmdSet,
+    kTUIOMsgCmdFSeq
+} TUIOMsgCmd;
+
+typedef struct {
+    float x;
+    float y;
+} TUIOMsgVec;
+
+typedef struct _TUIOMsg {
+    TUIOMsgType type;
+    TUIOMsgCmd cmd;
+    union _data {
+        struct {    // for cmd = "source"
+            char * addr;
+        } source;
+        struct {    // for cmd = "alive"
+            int * sessIds;
+            unsigned int numSessIds;
+        } alive;
+        struct {    // for cmd = "set"
+            int sessId;
+            int classId;
+            TUIOMsgVec pos;
+            float angle;
+            TUIOMsgVec size;
+            float area;
+            TUIOMsgVec vel;
+            float angleVel;
+            float motAccel;
+            float rotAccel;
+        } set;
+        struct {    // for cmd = "fseq"
+            int frameId;
+        } fseq;
+    } data;
+    
+    _TUIOMsg() {
+        memset(this, 0, sizeof(_TUIOMsg));
+    };
+    
+    ~_TUIOMsg() {
+        if (cmd == kTUIOMsgCmdSrc && data.source.addr) {
+            delete [] data.source.addr;
+            data.source.addr = NULL;
+        } else if (cmd == kTUIOMsgCmdAlive && data.alive.sessIds) {
+            delete [] data.alive.sessIds;
+            data.alive.sessIds = NULL;
+        }
+    };
+} TUIOMsg;
+
+typedef void(*TUIOMsgCallback)(TUIOMsg);
 
 class TUIOMsgListener : public osc::OscPacketListener {
 
 public:
     TUIOMsgListener(TUIOMsgCallback callback);
+    void setTUIOAddressPrefix(const char *newPrefix);
 
 protected:
     virtual void ProcessMessage(const osc::ReceivedMessage& m, const IpEndpointName& remoteEndpoint);
     
 private:
     TUIOMsgCallback msgCallback;
-    
+    const char *tuioAddressPrefix;
 };
 
 #endif
